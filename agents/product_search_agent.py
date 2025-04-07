c102cb31d4f9b0df4e8d5b064480a940119690e4
JnkKnd <kndjnk@outlook.jp>
2025-04-07 15:26:58 +0900

    update md

--- agents/product_search_agent.py
+++ agents/product_search_agent.py
@@ -5,14 +5,6 @@ from dotenv import load_dotenv
 from azure.identity import DefaultAzureCredential
 from typing import Any, Callable, Set, Dict, List, Optional
 from azure.ai.projects import AIProjectClient
-from azure.ai.projects.models import (
-    FunctionTool,
-    ToolSet,
-    CodeInterpreterTool,
-    AzureAISearchTool,
-    BingGroundingTool,
-    MessageTextContent,
-)
 from azure.search.documents import SearchClient, VectorizedQuery
 from openai import AzureOpenAI
 
@@ -22,79 +14,41 @@ model client を引数に定義する
 
 # 環境変数の読み込み
 load_dotenv()
-PROJECT_CONNECTION_STRING = os.getenv('PROJECT_CONNECTION_STRING')
-AZURE_OPENAI_KEY = os.getenv("AZURE_OPENAI_KEY")
-AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
-DEPLOYMENT_NAME = os.getenv("DEPLOYMENT_NAME")
-EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL")
 AI_SEARCH_ENDPOINT = os.getenv("AI_SEARCH_ENDPOINT")
 INDEX_NAME = os.getenv("INDEX_NAME")
-AI_SEARCH_CRED = os.getenv("AI_SEARCH_CRED")
-
-
-if not PROJECT_CONNECTION_STRING:
-    raise ValueError("'.env' に PROJECT_CONNECTION_STRING が設定されていません。")
-
-project_client = AIProjectClient.from_connection_string(
-    credential=DefaultAzureCredential(), conn_str=PROJECT_CONNECTION_STRING
-)
-if not PROJECT_CONNECTION_STRING:
-    raise ValueError("'.env' に PROJECT_CONNECTION_STRING が設定されていません。")
-
+AI_SEARCH_KEY = os.getenv("AI_SEARCH_KEY")
 
 search_client = SearchClient(
-    endpoint=AI_SEARCH_ENDPOINT, index_name=INDEX_NAME, credential=AI_SEARCH_CRED
+    endpoint=AI_SEARCH_ENDPOINT, index_name=INDEX_NAME, credential=AzureKeyCredential(AI_SEARCH_KEY)
 )
 
-aoai_client = AzureOpenAI(
-    api_key=AZURE_OPENAI_KEY,
-    api_version="2024-02-01",
-    azure_endpoint=AZURE_OPENAI_ENDPOINT,
-)
+def nonewlines(s: str) -> str:
+    return s.replace("\n", " ").replace("\r", " ").replace("[", "【").replace("]", "】")
 
 # Azure AI Search による検索
-def search_product_info(user_query: str)-> str :
+def search_product_info(query: str) -> str:
     """
-    Get the current time as a JSON string, optionally formatted.
+    保険の商品に関する質問に関して、Azure AI Search の検索結果を返します。
 
-    :param format (Optional[str]): 時刻のフォーマット。指定がない場合は "%Y-%m-%d %H:%M:%S" を使用
-    :return: Json フォーマットの現在時刻
+    :param query (str): 保険の商品を検索する際のクエリ
+    :rtype: str
+
+    :return: JSON 文字列の検索結果の情報
     :rtype: str
     """
 
-    vector_query = aoai_client.embeddings.create(
-        input=[user_query], model=EMBEDDING_MODEL
-    ).data[0].embedding
-
-    retrieved_docs = search_client.search(
-        search_text=user_query,
-        vector_queries=[vector_query],
-        top=3,
-        highlight_fields="content-3",
-        select=["sourcepage", "content", "category"],
-        query_type="semantic",
-        semantic_configuration_name="default",
+    results = search_client.search(
+        search_text=query,
+        query_type="semantic",                      # セマンティック検索を有効化
+        semantic_configuration_name="insurance-product-info-semantic-configuration",  # セマンティック検索用の構成名（環境に合わせて変更
+        top=3                                       # 上位5件を取得（必要に応じて変更）
     )
+    context = [nonewlines(doc['chunk']) for doc in results]
+    context_json = json.dumps({"context": context})
+    print(context_json)
+    return context_json
 
-    semantic_answers = retrieved_docs.get_answers()
-    print("semantic answer = ", semantic_answers)
-    for answer in semantic_answers:
-        if answer.highlights:
-            print(f"Semantic Answer: {answer.highlights}")
-        else:
-            print(f"Semantic Answer: {answer.text}")
-        print(f"Semantic Answer Score: {answer.score}\n")
 
-    for doc in retrieved_docs:
-        captions = doc["@search.captions"]
-        if captions:
-            caption = captions[0]
-            if caption.highlights:
-                print(f"Caption: {caption.highlights}\n")
-            else:
-                print(f"Caption: {caption.text}\n")
-
-    return None
 
 
 # ユーザ関数のセットアップ
--- ex1.md
+++ ex1.md
@@ -8,6 +8,8 @@
 - Bing Search Grounding の登録
 - エージェントからツールの呼び出し
 
+![alt text](images/image11.png)
+
 
 ## 演習 1-1 保険商品に関するインデックスを作成する
 
@@ -16,7 +18,7 @@
 2. Storage にアップロード
 
 3. Azure AI Search でのインデックス化
-![alt text](images/image21.png)
+![alt text](images/image12.png)
 
 - 手動でRBACの割り当てが必要
 - Azure AI Foundry Hub リソース作成時にできたストレージに対して、Azure AI Seardh の Blob データ閲覧者権限を付与する
@@ -25,6 +27,13 @@
 ## 演習 1-2 保険商品案内エージェントの作成
 <!-- .py でモジュール化すればいいのかしら -->
 1. VScode で agents フォルダを作成する
+
+## 演習 1-2 関数の定義
+- Azure AI Search の検索
+- Code Interpreter 追加
+- Bing Search
+
+## 演習 1-3 動作の確認
 
 <br>
 
